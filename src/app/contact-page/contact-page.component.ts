1f394039d651b801208a41c428d9416af49aa592
Danielle <xdanielle.dsouza@gmail.com>
2019-11-16 09:17:29 -0500

    Adding form functions

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -6,10 +6,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent implements OnInit {
+  public iftask: boolean = false;
+  iftask1: boolean = false;
+  iftask2: boolean = false;
+  iftask3: boolean = false;
+  iftask4: boolean = false;
+  iftask5: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  smallText(refVar: string) {
+    let val = refVar;
+    if (val.length > 0) {
+      this.iftask = true;
+    } else {
+      this.iftask = false;
+    }
+  }
+
+  smallText2(refVar: string) {
+    let val = refVar;
+    if (val.length > 0) {
+      this.iftask2 = true;
+    } else {
+      this.iftask2 = false;
+    }
+  }
+
+  smallText3(refVar: string) {
+    let val = refVar;
+    if (val.length > 0) {
+      this.iftask3 = true;
+    } else {
+      this.iftask3 = false;
+    }
+  }
+
+  smallText4(refVar: string) {
+    let val = refVar;
+    if (val.length > 0) {
+      this.iftask4 = true;
+    } else {
+      this.iftask4 = false;
+    }
+  }
+
+  smallText5(refVar: string) {
+    let val = refVar;
+    if (val.length > 0) {
+      this.iftask5 = true;
+    } else {
+      this.iftask5 = false;
+    }
+  }
 }
